0d3af52b5091ae870378c2821afba366dc49c96d
Aliaksandr Minkevich <minkeviiich@gmail.com>
2022-11-24 23:06:15 +0300

    Corrections to PR

--- homework3/task_three.py
+++ homework3/task_three.py
@@ -1,6 +1,6 @@
 import random
-randomlist = [random.randint(-100, 100) for x in range(10)] 
+randomlist = [random.randint(-100, 100) for x in range(10)]
 print(randomlist)
-randomlist[3] = random.randint(-100, 100) 
+randomlist[3] = random.randint(-100, 100)
 randomlist.pop(6)
 print(randomlist)
